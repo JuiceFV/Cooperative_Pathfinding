1253f0a1031d9219edc20fe61364cb4119f6ab08
JuiceFV <alexcar19622000@gmail.com>
2019-11-13 18:01:40 +0300

    Node modified

--- includes/common.hpp
+++ includes/common.hpp
@@ -3,24 +3,15 @@
 using namespace std;
 using uint = unsigned int;
 
+typedef struct Node {
+  uint x, y, f_score, g_score;
 
-typedef	struct  Node
-{
-	uint x, y, f_score, g_score;
+  bool operator==(const Node &n) const { return (x == n.x && y == n.y); }
 
-	bool operator==(const Node& n) const
-	{
-		return (x == n.x && y == n.y);
-	} 
+  bool operator!=(const Node &n) const { return (x != n.x || y != n.y); }
 
-	bool operator!=(const Node& n) const
-	{
-		return (x != n.x || y != n.y);
-	}
-
-	friend ostream& operator<<(ostream& out, const Node& n)
-	{
-		out << '[' << n.x << ", " << n.y << ']';
-		return (out);
-	}
+  friend ostream &operator<<(ostream &out, const Node &n) {
+    out << '[' << n.x << ", " << n.y << ']';
+    return (out);
+  }
 } Node;

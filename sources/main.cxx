661fbece7365b0d8b3256315962b417cd51f3349
JuiceFV <alexcar19622000@gmail.com>
2019-11-13 18:03:42 +0300

    .clang-format was aplied

--- sources/main.cxx
+++ sources/main.cxx
@@ -1,27 +1,26 @@
 #include <iostream>
+
 #include "common.hpp"
 
-static void set_node(Node& n, uint x, uint y)
-{
-	n.x = x;
-	n.y = y;
+static void set_node(Node &n, uint x, uint y) {
+  n.x = x;
+  n.y = y;
 }
-
-int main()
-{
-	Node a, b;
-	set_node(a, 1, 2);
-	set_node(b, 1, 2);
-	std::cout << "a: " << a << std::endl << "b: " << b << endl;
-	a == b ? std::cout << "a equal b" : std::cout << "a does not equal b";
-	std::cout << std::endl;
-	std::cout << std::endl;
-	set_node(b, 5, 6);
-	std::cout << "a: " << a << std::endl << "b: " << b << endl;
-	a != b ? std::cout << "a does not equal b" : std::cout << "a equal b";
-	std::cout << std::endl;
-	#if defined(_MSC_VER) || defined(_WIN32)
-		system("pause");
-	#endif
-	return (0);
+// huilo
+int main() {
+  Node a, b;
+  set_node(a, 1, 2);
+  set_node(b, 1, 2);
+  std::cout << "a: " << a << std::endl << "b: " << b << endl;
+  a == b ? std::cout << "a equal b" : std::cout << "a does not equal b";
+  std::cout << std::endl;
+  std::cout << std::endl;
+  set_node(b, 5, 6);
+  std::cout << "a: " << a << std::endl << "b: " << b << endl;
+  a != b ? std::cout << "a does not equal b" : std::cout << "a equal b";
+  std::cout << std::endl;
+#if defined(_MSC_VER) || defined(_WIN32)
+  system("pause");
+#endif
+  return (0);
 }

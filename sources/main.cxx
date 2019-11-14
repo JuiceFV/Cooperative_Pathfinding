190baf604def5743c14217930e180df9473f3822
JuiceFV <alexcar19622000@gmail.com>
2019-11-14 21:54:39 +0300

    Just added print_list maintenaning function

--- sources/main.cxx
+++ sources/main.cxx
@@ -1,24 +1,18 @@
-#include <iostream>
-
+#include "../tests/tests.hpp"
+#include "Agent.hpp"
 #include "common.hpp"
 
-static void set_node(Node &n, uint x, uint y) {
+static void set_node(Node& n, uint x, uint y) {
   n.x = x;
   n.y = y;
 }
 
+static void print_list(list<int> l) {
+  for (auto& i : l) cout << i << ' ';
+  cout << endl;
+}
 int main() {
-  Node a, b;
-  set_node(a, 1, 2);
-  set_node(b, 1, 2);
-  std::cout << "a: " << a << std::endl << "b: " << b << endl;
-  a == b ? std::cout << "a equal b" : std::cout << "a does not equal b";
-  std::cout << std::endl;
-  std::cout << std::endl;
-  set_node(b, 5, 6);
-  std::cout << "a: " << a << std::endl << "b: " << b << endl;
-  a != b ? std::cout << "a does not equal b" : std::cout << "a equal b";
-  std::cout << std::endl;
+ 
 #if defined(_MSC_VER) || defined(_WIN32)
   system("pause");
 #endif
